65e7c445955e4f4c78a77743494c48b320d97190
leeboo <libao@peilian.com>
2021-04-12 15:21:57 +0800

    接入相机扫码

--- app/src/main/java/com/vip/pda/StockActivity.java
+++ app/src/main/java/com/vip/pda/StockActivity.java
@@ -1,5 +1,7 @@
 package com.vip.pda;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -12,15 +14,24 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.king.zxing.CameraConfig;
+import com.king.zxing.CameraScan;
+import com.king.zxing.CaptureActivity;
+import com.king.zxing.DecodeConfig;
+import com.king.zxing.DefaultCameraScan;
+import com.king.zxing.analyze.MultiFormatAnalyzer;
 import com.vip.pda.bean.StockBean;
 import com.vip.pda.file.SPUtils;
 import com.vip.pda.file.SharePopup;
 import com.vip.pda.http.ApiDisposableObserver;
 import com.vip.pda.http.BaseResponse;
 import com.vip.pda.http.RetrofitClient;
+import com.vip.pda.utils.CommonUtils;
 import com.vip.pda.utils.ToastUtils;
 
 import java.util.ArrayList;
@@ -49,6 +60,8 @@ public class StockActivity extends AppCompatActivity {
     List<String> list = new ArrayList<>();
     String titleText, userKey;
     SharePopup popup;
+    public static final int REQUEST_CODE_SCAN = 0X01;
+    public static final int REQUEST_CODE_PHOTO = 0X02;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -90,7 +103,7 @@ public class StockActivity extends AppCompatActivity {
                     return true;
                 }
                 //在这里实现自己的逻辑代码
-                if (TextUtils.isEmpty(tvDh.getText().toString())) {
+                if (titleText.contains("出库") && TextUtils.isEmpty(tvDh.getText().toString())) {
                     tvDh.setText(barcode);
                 } else {
                     list.add(barcode);
@@ -101,24 +114,38 @@ public class StockActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * 检测拍摄权限
+     */
+    private void checkCameraPermissions() {
+        if (CommonUtils.checkHasPermission(this, Manifest.permission.CAMERA)) {
+            Intent intent = new Intent(this, CaptureActivity.class);
+            ActivityCompat.startActivityForResult(this, intent, REQUEST_CODE_SCAN, null);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
+    }
+
     @OnClick({R.id.mock, R.id.add, R.id.commit})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.mock:
-                Random random = new Random();
-                String[] c = {"111122223333", "222233331111", "333311112222", "333322221111", "444422221111", "555522221111"};
-                String[] s = {"(01)90.GN.2001B.5033.C000003;(03)1;(07)OA01000398;(05)21040900002",
-                        "(01)90.GN.2001B.9107.C000002;(03)1;(07)OA01000398;(05)21040900003",
-                        "(01)90.GN.2001B.1762.C000003;(03)1;(07)OA01000398;(05)21040900004",
-                        "(01)90.GN.2001B.9007.C000001;(03)1;(07)OA01000398;(05)21040900005",
-                        "(01)90.GN.2001B.5033.C000003;(03)1;(07)OA01000398;(05)21040900006",
-                        "(01)90.GN.2001B.9007.C000001;(03)1;(07)OA01000398;(05)21040900007"};
-                if (TextUtils.isEmpty(tvDh.getText().toString())) {
-                    tvDh.setText(c[random.nextInt(5)]);
-                } else {
-                    list.add(s[random.nextInt(5)]);
-                    adapter.insert(s[random.nextInt(5)]);
-                }
+//                Random random = new Random();
+//                String[] c = {"BDO111122223333", "BDO222233331111", "BDO333311112222",
+//                        "BDO333322221111", "BDO444422221111", "BDO555522221111"};
+//                String[] s = {"(01)90.GN.2001B.5033.C000003;(03)1;(07)OA01000398;(05)21040900002",
+//                        "(01)90.GN.2001B.9107.C000002;(03)1;(07)OA01000398;(05)21040900003",
+//                        "(01)90.GN.2001B.1762.C000003;(03)1;(07)OA01000398;(05)21040900004",
+//                        "(01)90.GN.2001B.9007.C000001;(03)1;(07)OA01000398;(05)21040900005",
+//                        "(01)90.GN.2001B.5033.C000003;(03)1;(07)OA01000398;(05)21040900006",
+//                        "(01)90.GN.2001B.9007.C000001;(03)1;(07)OA01000398;(05)21040900007"};
+//                if (titleText.contains("出库") && TextUtils.isEmpty(tvDh.getText().toString())) {
+//                    tvDh.setText(c[random.nextInt(5)]);
+//                } else {
+//                    list.add(s[random.nextInt(5)]);
+//                    adapter.insert(s[random.nextInt(5)]);
+//                }
+                checkCameraPermissions();
                 break;
             case R.id.add:
                 popup.showAtLocation(title, Gravity.BOTTOM, 0, 0);
@@ -160,5 +187,23 @@ public class StockActivity extends AppCompatActivity {
         bean.setDh(tvDh.getText().toString());
         bean.setList(list);
         SPUtils.getInstance(userKey).putObject(titleText + "|" + tvDh.getText().toString(), bean);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null) {
+            switch (requestCode) {
+                case REQUEST_CODE_SCAN:
+                    String result = CameraScan.parseScanResult(data);
+                    if (titleText.contains("出库") && TextUtils.isEmpty(tvDh.getText().toString())) {
+                        tvDh.setText(result);
+                    } else {
+                        list.add(result);
+                        adapter.insert(result);
+                    }
+                    break;
+            }
+        }
     }
 }
